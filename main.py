b87a4c3e0c4676db36638a86bf150beb55ae57bd
Sanara-Perera <sanaraperera2003@gmail.com>
2026-03-20 01:19:38 +0530

    Add vehicles statistics endpoint

--- main.py
+++ main.py
@@ -79,3 +79,16 @@ def filter_vehicles(
         raise HTTPException(status_code=404, detail="No vehicles found for the given filters")
  
     return data.to_dict(orient="records")
+
+@app.get("/vehicles/stats")
+def get_stats():
+    data = load_vehicles()
+    numeric_price = pd.to_numeric(data["Price"], errors='coerce')
+    return {
+        "total_vehicles": len(data),
+        "average_price": round(numeric_price.mean(), 2),
+        "min_price": round(numeric_price.min(), 2),
+        "max_price": round(numeric_price.max(), 2),
+        "total_makes": data["Make"].nunique(),
+        "total_districts": data["District"].nunique(),
+    }
